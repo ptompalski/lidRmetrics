95b4d455760fb1bb4c7e4d35302f2aeafe24e726
Tompalski <piotr.tompalski@nrcan-rncan.gc.ca>
2024-02-29 14:35:35 -0800

    fixing #11

--- R/metrics_voxels.R
+++ R/metrics_voxels.R
@@ -51,43 +51,47 @@ metrics_voxels <- function(x, y, z, vox_size=1, zmin = NA) {
     
     vox <- lidRmetrics:::create_voxels(x = x, y = y, z = z, vox_size = vox_size, zmin = zmin)
     
-    vox_filled <- vox[!is.na(vox$n),]
+    #if not null (i.e. if there were enough points to create voxels)
     
-    vn <- nrow(vox_filled)
+    if(!is.null(vox)) {
+      
+      vox_filled <- vox[!is.na(vox$n),]
+      
+      vn <- nrow(vox_filled)
+      
+      # FR (filling ratio, number of voxels with data to the number of all possible voxels)
+      vFRall <- nrow(vox_filled) / nrow(vox) * 100
+      
+      #FR under canopy (limit the filling ratio calculations with the top of the canopy)
+      vox2 <- vox %>%
+        dplyr::filter(!is.na(n)) %>%         #remove empty voxels
+        dplyr::group_by(X, Y) %>%             #for each X and Y...
+        dplyr::summarise(zmax = max(Z), .groups = "keep") %>%   #...find the highest voxel
+        dplyr::right_join(vox, by=c("X", "Y")) %>%            #combine with original voxel data
+        dplyr::filter(Z <= zmax)              #remove empty voxels above canopy
+      
+      vFRcanopy <- nrow(vox2[!is.na(vox2$n),])  / nrow(vox2) * 100
+      
+      mhist <- lidRmetrics:::metrics_voxstructure(z = vox_filled$Z, vox_size = vox_size)
+      
+      mlefsky <- lidRmetrics:::metrics_lefsky(x = vox$X, y = vox$Y, z = vox$Z, n=vox$n)
+      
+    } else {
+      
+      mhist <- lidRmetrics:::metrics_voxstructure(z = NA) #this is temporary fix
+      mlefsky <- lidRmetrics:::metrics_lefsky(x = NA, y = NA, z = NA, n=NA) #this is temporary fix
+      
+    }
     
-    # FR (filling ratio, number of voxels with data to the number of all possible voxels)
-    vFRall <- nrow(vox_filled) / nrow(vox) * 100
     
-    #FR under canopy (limit the filling ratio calculations with the top of the canopy)
-    vox2 <- vox %>%
-      dplyr::filter(!is.na(n)) %>%         #remove empty voxels
-      dplyr::group_by(X, Y) %>%             #for each X and Y...
-      dplyr::summarise(zmax = max(Z), .groups = "keep") %>%   #...find the highest voxel
-      dplyr::right_join(vox, by=c("X", "Y")) %>%            #combine with original voxel data
-      dplyr::filter(Z <= zmax)              #remove empty voxels above canopy
+    output = list(vn = vn,
+                  vFRall = vFRall,
+                  vFRcanopy = vFRcanopy)
     
-    vFRcanopy <- nrow(vox2[!is.na(vox2$n),])  / nrow(vox2) * 100
+    output <- c(output, mhist, mlefsky)
     
-    mhist <- lidRmetrics:::metrics_voxstructure(z = vox_filled$Z, vox_size = vox_size)
-    
-    mlefsky <- lidRmetrics:::metrics_lefsky(x = vox$X, y = vox$Y, z = vox$Z, n=vox$n)
-    
-  } else {
-    
-    mhist <- lidRmetrics:::metrics_voxstructure(z = NA) #this is temporary fix
-    mlefsky <- lidRmetrics:::metrics_lefsky(x = NA, y = NA, z = NA, n=NA) #this is temporary fix
-    
+    return(output)
   }
-  
-  
-  output = list(vn = vn,
-                vFRall = vFRall,
-                vFRcanopy = vFRcanopy)
-  
-  output <- c(output, mhist, mlefsky)
-  
-  return(output)
-  
   
 }
 
--- R/utils.R
+++ R/utils.R
@@ -11,81 +11,102 @@ create_voxels <- function(x, y, z, vox_size=1, zmin = NA) {
   ) 
   
   if (!is.na(zmin)) {
-    D <- D %>% filter(Z >= zmin)
+    D <- D[D$Z>zmin,]
+    # D <- D %>% filter(Z >= zmin) #the culprit
   }
   
-  V <- D %>%
-    dplyr::group_by(X, Y, Z) %>%
-    dplyr::count()
+  #continue only D is not empty
   
-  data.table::setDT(V)
-  data.table::setkey(V, X, Y, Z)
-  
-  xrange <- range(V$X)
-  yrange <- range(V$Y)
-  zrange <- range(V$Z)
-  
-  V_all <- expand.grid(
-    X = seq(xrange[1], xrange[2], by = vox_size),
-    Y = seq(yrange[1], yrange[2], by = vox_size),
-    Z = seq(zrange[1], zrange[2], by = vox_size)
-  )
-  
-  data.table::setDT(V_all)
-  data.table::setkey(V_all, X, Y, Z)
-  
-  V <- merge(V, V_all, all = TRUE)
-  
-  return(V)
-  
+  if(nrow(D) > 0) {
+    
+    V <- D %>%
+      dplyr::group_by(X, Y, Z) %>%
+      dplyr::count()
+    
+    data.table::setDT(V)
+    data.table::setkey(V, X, Y, Z)
+    
+    xrange <- range(V$X, na.rm=T)
+    yrange <- range(V$Y, na.rm=T)
+    zrange <- range(V$Z, na.rm=T)
+    
+    V_all <- expand.grid(
+      X = seq(xrange[1], xrange[2], by = vox_size),
+      Y = seq(yrange[1], yrange[2], by = vox_size),
+      Z = seq(zrange[1], zrange[2], by = vox_size)
+    )
+    
+    data.table::setDT(V_all)
+    data.table::setkey(V_all, X, Y, Z)
+    
+    V <- merge(V, V_all, all = TRUE)
+    
+    return(V)
+  }
 }
 
 
 ### vertical rumple and other histogram variability metrics ####
 # to be used with metrics_voxels()
+# metrics_voxstructure <- function(z, vox_size) {
+#   
+#   vzrumple <- vzsd <- vzcv <- NA_real_
+#   
+#   if (length(z) > 2) {
+#     
+#     from=min(z, na.rm=T)
+#     to=max(z, na.rm=T)
+#     
+#   }
+#   
+#   output <- list(from=from, to=to)
+#   return(output)
+# }
+
+
 
 metrics_voxstructure <- function(z, vox_size) {
-  
+
   vzrumple <- vzsd <- vzcv <- NA_real_
-  
+
   if (length(z) > 2) {
-    
+
     # define breaks
-    brks <- seq(from=min(z), to=max(z), by=vox_size)
-    
+    brks <- seq(from=min(z, na.rm=T), to=max(z, na.rm=T), by=vox_size)
+
     if (length(brks) > 1) {
-      
+
       # calculate frequencies
       v_hist <- hist(z, breaks=brks,  plot=F)
-      
+
       # normalize frequencies (0,1)
       freq <- v_hist$counts
       freq <- (freq-min(freq))/(max(freq)-min(freq))
-      
-      #vertical rumple 
-      # flength <- sum(sqrt(abs(lag(freq) - freq)^2 + vox_size^2), na.rm=T) 
+
+      #vertical rumple
+      # flength <- sum(sqrt(abs(lag(freq) - freq)^2 + vox_size^2), na.rm=T)
       euclidean <- function(a, b) sqrt(sum((a - b)^2))
-      
+
       flength = euclidean(freq, brks[1:length(freq)])
       fheight <- length(freq)*vox_size
       vzrumple <- flength/fheight
-      
+
       #other metrics
       vzsd <- sd(freq)
       vzcv <- vzsd / mean(freq) * 100
-      
-      
-      
+
+
+
     }
   }
-  
+
   output <- list(
-    vzrumple = vzrumple, 
-    vzsd= vzsd, 
+    vzrumple = vzrumple,
+    vzsd= vzsd,
     vzcv= vzcv
   )
-  
-  
-  
+
+
+
   return(output)
 }
